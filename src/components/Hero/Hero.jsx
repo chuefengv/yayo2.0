3065771433c6699032d96314123ac472cdc807be
chuefengv <feng.vang98@gmail.com>
2022-11-09 12:11:01 -0500

    hero mobile and desktop files added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./Hero.css"
+import HeroDesktop from './HeroDesktop'
+import HeroMobile from './HeroMobile'
 
 export default function Hero() {
   return (
-    <div className="hero-container">Hero</div>
+    <div className="hero-container">
+        Hero
+        <HeroMobile />
+        <HeroDesktop />
+    </div>
   )
 }
---
+++ src/components/Hero/HeroMobile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HeroMobile() {
+  return (
+    <div>HeroMobile</div>
+  )
+}
