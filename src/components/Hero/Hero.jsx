ac970998a6622284cb09f7415184ce5cb2b6ef34
chuefengv <feng.vang98@gmail.com>
2022-11-09 12:46:00 -0500

    added event lsitener to switch between mobile and desktop compoents

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import "./Hero.css"
 import HeroDesktop from './HeroDesktop'
 import HeroMobile from './HeroMobile'
+import { useEffect, useState } from 'react'
 
 export default function Hero() {
+
+    const [deskMode, setDeskMode] = useState((window.innerWidth>=950) ? true : false);
+    
+    useEffect(()=>{
+        function renderFeed(){
+            window.innerWidth>=950 ? setDeskMode(true) : setDeskMode(false)
+        }
+        window.addEventListener('resize',renderFeed)
+      })
+
   return (
     <div className="hero-container">
-        Hero
-        <HeroMobile />
-        <HeroDesktop />
+        {deskMode ? <HeroDesktop />:<HeroMobile />}
     </div>
   )
 }
